7089e32eb0f98cb4a0aa5d2248d62676192eb4a8
TonyNiuCOM <Tony@MacBook-Pro-2.local>
2016-12-05 01:25:38 +0100

    backend for BB

---
+++ BuzzBrother/routes/web.php
@@ -0,0 +1,28 @@
+
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Log;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+//http://e-howtogeek.com/710944/problem-page-found-returns-http-status-code-instead-laravel
+
+Route::get('/', function (Request $request) {
+    $input = $request->all();
+    $ps = $request->input('passcode');
+
+    if ($ps === 'BuzzAR')
+    return response('OK', 200)
+        ->header('Content-Type', 'text/plain');
+    else response('NOT OK', 200)
+        ->header('Content-Type', 'text/plain');
+});
+
+
